4117193504aeb11780269701841804200289d4ac
M. Thoriq Al Fajri <83461390+thoriqalfajri@users.noreply.github.com>
2021-05-02 19:12:09 +0700

    Update findwords.cpp

--- findwords.cpp
+++ findwords.cpp
@@ -26,57 +26,35 @@ char *reverse(char *);
 
 bool searchHorizontal(char masuk[]){
 	char *liat;
-	char kata[15];
-	
+	char kata[15]; 
 	for (int i=0;i<15;i++){
-    	for(int j=0;j<15;j++){
-	    	kata[j] = words[i][j];
-		}
 		liat = strstr(words[i], masuk);
 	    if(liat != NULL){
 	        return true;
 	    }
-	    else{
+    	for(int j=0;j<15;j++){	
+	    	kata[j] = words[i][j];
+    	}
 	    	reverse(kata, kata + strlen(kata));
 	        liat = strstr(kata, masuk); 
 	        if(liat != NULL){ 
 	            return true; 
-	        }
-		} 
+	        }	
     }
     return false; 
 }
 
-bool searchVertical(char masuk[]){
-	char *liat;
-	char kata[15];
-	for (int i = 0; i < 15; i++){
-        for (int j = 0; j < 15; j++){
-            kata[j] = words[j][i];
-        }
-        liat = strstr(kata, masuk);
-        if(liat != NULL){
-	        return true;
-	    }
-	    else{
-	    	reverse(kata, kata + strlen(kata)); 
-            liat = strstr(kata, masuk);
-            if(liat != NULL){
-	          return true;
-	        }
-		}
-    }
-    return false;
+bool searchVertical(char \){
+	
 }
 
 int main()
 {
     char word[16];
     int n;
-    cin.ignore();
     cin>>n;
+    cin.ignore();
     for (int i=0;i<n;i++){
-    	cin.ignore();
         cin.getline(word,16,'\n');
         if (searchHorizontal(word) || searchVertical(word)){
             cout << "Ada\n";
